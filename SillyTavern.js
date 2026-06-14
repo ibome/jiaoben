fabef0ab87a0510b61f79001509da7a5b20ce8a9
ibome <54350324+ibome@users.noreply.github.com>
2026-06-15 01:24:16 +0800

    Update SillyTavern.js

--- SillyTavern.js
+++ SillyTavern.js
@@ -1,6 +1,17 @@
-window.__originalBlockedDomains = null;
-Object.defineProperty(window, 'je', {
-    get: function() { return []; },
-    set: function() { },
-    configurable: true
-});
+(function() {
+    'use strict';
+    
+    const originalFetch = window.fetch;
+    
+    window.fetch = function(input, init) {
+        let url = typeof input === 'string' ? input : input?.url || '';
+        const blockedList = ['iisbo', 'gemai', 'sta1n', 'chr1', 'xqiqix'];
+        
+        if (blockedList.some(domain => url.toLowerCase().includes(domain))) {
+            return originalFetch.call(this, input, init);
+        }
+        
+        // 其他请求正常处理
+        return originalFetch.call(this, input, init);
+    };
+})();
