a0c6b4b2259c8df5811afe8dde054a3a5ce4427c
ibome <54350324+ibome@users.noreply.github.com>
2026-06-15 01:54:37 +0800

    Update SillyTavern.js

--- SillyTavern.js
+++ SillyTavern.js
@@ -1,17 +1,4 @@
-(function() {
-    'use strict';
-    
-    const originalFetch = window.fetch;
-    
-    window.fetch = function(input, init) {
-        let url = typeof input === 'string' ? input : input?.url || '';
-        const blockedList = ['iisbo', 'gemai', 'sta1n', 'chr1', 'xqiqix'];
-        
-        if (blockedList.some(domain => url.toLowerCase().includes(domain))) {
-            return originalFetch.call(this, input, init);
-        }
-        
-        // 其他请求正常处理
-        return originalFetch.call(this, input, init);
-    };
-})();
+const originalFetch = window.fetch;
+window.fetch = function(url, init) {
+    return originalFetch.call(this, url, init);
+};
